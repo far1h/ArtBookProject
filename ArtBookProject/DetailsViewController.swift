32142bdae810c90b34faf00fcac602554c26ddba
far1h <farihmuhammad45@gmail.com>
2024-08-26 22:11:50 +0700

    Add UI Elements

---
+++ ArtBookProject/DetailsViewController.swift
@@ -0,0 +1,27 @@
+//
+//  DetailsViewController.swift
+//  ArtBookProject
+//
+//  Created by Farih Muhammad on 26/08/2024.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameText: UITextField!
+    @IBOutlet weak var artistText: UITextField!
+    @IBOutlet weak var yearText: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func saveButtonClicked(_ sender: UIButton) {
+        
+    }
+    
+}
