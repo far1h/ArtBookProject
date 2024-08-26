72b288a4bcee391803f3d06bdfa70374ee93e260
far1h <farihmuhammad45@gmail.com>
2024-08-26 22:26:08 +0700

    Add + top bar button programmatically

--- ArtBookProject/ViewController.swift
+++ ArtBookProject/ViewController.swift
@@ -14,8 +14,12 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addButtonClicked))
     }
 
-
+    @objc func addButtonClicked(){
+        performSegue(withIdentifier: "toDetailsVC", sender: nil)
+    }
 }
 
