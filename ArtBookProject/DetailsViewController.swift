04637187255db57e905ec220efe53e08f7e098fb
far1h <farihmuhammad45@gmail.com>
2024-08-27 16:24:13 +0700

    Implement create/storing data functionality

--- ArtBookProject/DetailsViewController.swift
+++ ArtBookProject/DetailsViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import CoreData
 
 class DetailsViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
@@ -27,6 +28,27 @@ class DetailsViewController: UIViewController, UIImagePickerControllerDelegate, 
     
     @IBAction func saveButtonClicked(_ sender: UIButton) {
         
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let newPainting = NSEntityDescription.insertNewObject(forEntityName: "Paintings", into: context)
+        
+        newPainting.setValue(nameText.text!, forKey: "name")
+        newPainting.setValue(artistText.text!, forKey: "artist")
+        if let year = Int(yearText.text!) {
+            newPainting.setValue(year, forKey: "year")
+        }
+        newPainting.setValue(UUID(), forKey: "id")
+        
+        let data = imageView.image!.jpegData(compressionQuality: 0.5)
+        
+        newPainting.setValue(data, forKey: "image")
+        do {
+            try context.save() 
+            print("success")
+        } catch {
+            print("error")
+        }
     }
     
     @objc func hideKeyboard(){
